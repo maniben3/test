53c3ab43995b9e8fb06ee1aaf3bac71e093b412f
maniben3 <106887188+maniben3@users.noreply.github.com>
2022-10-05 23:21:49 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ for i in range(2,10**9):
   r=8*t*i-1
   s=16*t*i-1
   a=32*t*i-1
-  #b=64*t*i+1
+  b=64*t*i+1
   #c=128*t*i+1
   #d=256*t*i+1
   #e=512*t*i+1
@@ -19,8 +19,11 @@ for i in range(2,10**9):
          if gmpy2.is_fermat_prp(r,2) ==1 :
             if gmpy2.is_fermat_prp(s,2) ==1 :
               print(i,"6")
+              print(time.time()-start)
               if gmpy2.is_fermat_prp(a,2) ==1 :
-                #if gmpy2.is_fermat_prp(b,2) ==1 :
+                print(i,"7")
+                print(time.time()-start)
+                if gmpy2.is_fermat_prp(b,2) ==1 :
                  # if gmpy2.is_fermat_prp(c,2) ==1 :
                   #  if gmpy2.is_fermat_prp(d,2) ==1 :
                    #  if gmpy2.is_fermat_prp(e,2) ==1 :
